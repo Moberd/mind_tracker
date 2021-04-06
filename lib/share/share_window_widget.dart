9db503be2d2b4f08cebf0a14de1cd0ccf315608a
Moberd <ocheretov.ilya@yandex.ru>
2021-04-06 13:54:03 +0300

    friends list started

--- lib/share/share_window_widget.dart
+++ lib/share/share_window_widget.dart
@@ -1,9 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
-
-///Правый экрас с возможностью поделиться и записями друзей
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:barcode_scan_fix/barcode_scan.dart';
@@ -25,21 +22,6 @@ class FriendsList extends StatefulWidget {
 }
 
 class _FriendsListState extends State<FriendsList> {
-  final _friends = [
-    'Friend1',
-    'Friend2',
-    'Friend3',
-    'Friend4',
-    'Friend5',
-    'Friend6',
-    'Friend7',
-    'Friend8',
-    'Friend9',
-    'Friend10',
-    'Friend11',
-    'Friend12'
-  ];
-  final _moods = [10, 4, 3, 10, 2, 4, 1, 7, 2, 9, 1, 10];
   final _titleFont =
       TextStyle(fontSize: 30.0, color: Color.fromRGBO(0, 0, 0, 1));
   final _mainFont =
@@ -71,32 +53,34 @@ class _FriendsListState extends State<FriendsList> {
             ]),
         body: _buildList());
   }
-
+  
+  List<dynamic> friends;
+  void getData(){
+    var mEmail = FirebaseAuth.instance.currentUser.email;
+    FirebaseFirestore.instance.collection('users').doc(mEmail).get().then((value) {
+      friends = value.data()["friends"];
+    });
+  }
   Widget _buildList() {
-    int c = 0; //Счётчик вывода дат
-    return ListView.builder(
+    /*return ListView.builder(
         padding: EdgeInsets.all(0.0),
-        itemCount: _friends.length + 3,
+        itemCount: _friends.length,
         itemBuilder: (context, i) {
-          final index = i - c;
-          if (i % 5 == 0) {
-            //Вывод даты
-            c += 1;
+          //тут дата
             return Padding(
               padding: const EdgeInsets.all(4),
               child: Container(
                   decoration: BoxDecoration(
                     color: Color(0xFFFEF9FF),
-                    //border: Border.all(color: Color(0xFF736CED), width: 1),
-                    //borderRadius: BorderRadius.circular(7)
                   ),
                   child: ListTile(
                     title: Text('Date (last visited)', style: _dateFont),
                   )),
             );
           }
+          //тут друг
           return _buildRow(_friends[index], _moods[index]);
-        });
+        });*/
   }
 
   Widget _buildRow(String text, int mood) {
@@ -161,7 +145,6 @@ class _FriendsListState extends State<FriendsList> {
       setState(() => {});
     }
   }
-
   void addFriend(String name) async {
     var mEmail = FirebaseAuth.instance.currentUser.email;
     FirebaseFirestore.instance
@@ -172,7 +155,6 @@ class _FriendsListState extends State<FriendsList> {
       List<dynamic> list = value.data()["friends"];
       list.add(name);
       applyChanges(list);
-      print(name);
     });
   }
 
@@ -182,6 +164,5 @@ class _FriendsListState extends State<FriendsList> {
         .collection('users')
         .doc(mEmail)
         .update({"friends": friendsList});
-    print(friendsList);
   }
 }
